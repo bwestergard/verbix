7af0247fda94696e050091aab58539b0b48fdf32
Bjorn Westergard <bjornw@gmail.com>
2015-11-13 15:38:06 -0500

    mkdir lib
    Signed-off-by: Bjorn Westergard <bjornw@gmail.com>

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 var R = require('ramda');
 var $http = require('http-as-promised');
 var fs = require('fs');
-var scrape = require('./scrape');
+var scrape = require('./lib/scrape');
 
 module.exports.conjugate = function (language, verb) {
   return $http('http://www.verbix.com/webverbix/' +
                language + '/' +
                verb + '.html').
     spread(R.pipe(
-      R.nthArg(1),
+      R.nthArg(1), // response, body
       scrape
     ));
 };
 
-module.exports.conjugate('German', 'schreien').
+module.exports.conjugate('German', 'mitbringen').
   then(R.curry(JSON.stringify)(R.__, null, 2)).
   then(console.log);
-
